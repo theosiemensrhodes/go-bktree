c604a2dd6662f3ea7ebf5c3a1ceec6cc0e2e8ace
Mahmud Ridwan <m@hjr265.me>
2015-01-31 22:00:12 +0600

    Initial commit

--- bktree.go
+++ bktree.go
@@ -1,31 +1,40 @@
+// Copyright 2014 Mahmud Ridwan. All rights reserved.
+
+// Package bktree provides an implementation of BK-tree (http://en.wikipedia.org/wiki/BK-tree).
+//
+// A BK-tree is a metric tree suggested by Walter Austin Burkhard and Robert M. Keller specifically adapted to discrete metric spaces.
 package bktree
 
+// The Metric type is a function used by BK-tree instances to measure the distance between two given strings.
 type Metric func(a, b string) int
 
+// BKTree represents a BK-tree with a given metric function.
 type BKTree struct {
-	Metric Metric
-
-	root *node
+	Metric Metric // Metric function, required
+	root   *node
 }
 
+// New returns an initialized BK-tree.
 func New(m Metric) *BKTree {
 	return &BKTree{
 		Metric: m,
 	}
 }
 
-func (t *BKTree) Add(s string) {
+// Add inserts a new word to the BK-tree.
+func (t *BKTree) Add(w string) {
 	if t.root == nil {
-		t.root = &node{s, make(map[int]*node)}
+		t.root = &node{w, make(map[int]*node)}
 	} else {
-		t.root.add(s, t.Metric)
+		t.root.add(w, t.Metric)
 	}
 }
 
-func (t *BKTree) Find(s string, n int) []string {
+// Find returns all the words in the BK-tree with a distance of n from w.
+func (t *BKTree) Find(w string, n int) []string {
 	r := []string{}
 	if t.root != nil {
-		r = t.root.find(s, n, -1, t.Metric, r)
+		r = t.root.find(w, n, -1, t.Metric, r)
 	}
 	return r
 }
@@ -35,17 +44,17 @@ type node struct {
 	childs map[int]*node
 }
 
-func (e *node) add(s string, m Metric) {
-	d := m(e.word, s)
+func (e *node) add(w string, m Metric) {
+	d := m(e.word, w)
 	if c, ok := e.childs[d]; !ok {
-		e.childs[d] = &node{s, make(map[int]*node)}
+		e.childs[d] = &node{w, make(map[int]*node)}
 	} else {
-		c.add(s, m)
+		c.add(w, m)
 	}
 }
 
-func (e *node) find(s string, n, d int, m Metric, r []string) []string {
-	l := m(e.word, s)
+func (e *node) find(w string, n, d int, m Metric, r []string) []string {
+	l := m(e.word, w)
 	if l <= n {
 		r = append(r, e.word)
 	}
@@ -54,7 +63,7 @@ func (e *node) find(s string, n, d int, m Metric, r []string) []string {
 	}
 	for i := n - d; i <= n+d; i++ {
 		if c, ok := e.childs[i]; ok {
-			r = c.find(s, n, d, m, r)
+			r = c.find(w, n, d, m, r)
 		}
 	}
 	return r
--- bktree_test.go
+++ bktree_test.go
@@ -1,3 +1,5 @@
+// Copyright 2014 Mahmud Ridwan. All rights reserved.
+
 package bktree
 
 import (
